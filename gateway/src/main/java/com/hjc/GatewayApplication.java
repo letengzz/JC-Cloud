c6f9397e294673ae0056ec5bedcc3bd7b0c23e90
letengzz <2020885569@qq.com>
2023-12-12 11:47:36 +0800

    jc-cloud initialize

---
+++ gateway/src/main/java/com/hjc/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.hjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Gateway 网关
+ * @author hjc
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
